1c257c027d1dd99804c907fa2d5bf22044ac3012
kaihchang <kevinchang0527@gmail.com>
2024-10-19 00:55:42 +0800

    first commit

---
+++ config.h
@@ -0,0 +1,53 @@
+// Copyright 2024 Kai H. Chang (@kaihchang)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+// rotary encoder
+#define ENCODER_RESOLUTION 4
+#define ENCODER_A_PINS { ADC0 }
+#define ENCODER_B_PINS { ADC1 }
+
+// SSD1306 OLED display
+#define MY_I2C_ADDRESS (0x3C << 1)
+#ifdef OLED_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 10000
+#endif
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the joystick.  See also:
+// - `AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+    #define AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#endif // POINTING_DEVICE_ENABLE
+
+#define LAYOUT( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, \
+              k42, k43, k44, k45, k46, k47            \
+) { \
+    { k00,   k01,   k02, k03, k04, k05, k06, k07, k08,   k09   }, \
+    { k10,   k11,   k12, k13, k14, k15, k16, k17, k18,   k19   }, \
+    { k20,   k21,   k22, k23, k24, k25, k26, k27, k28,   k29   }, \
+    { k30,   k31,   k32, k33, k34, k35, k36, k37, k38,   k39   }, \
+    { KC_NO, KC_NO, k42, k43, k44, k45, k46, k47, KC_NO, KC_NO }  \
+}
---
+++ keymaps/default/keymap.c
@@ -0,0 +1,61 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+#ifdef AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#    include "timer.h"
+#endif // AUTO_POINTER_LAYER_TRIGGER_ENABLE
+
+enum custom_keycodes {
+  LAYER_QWERTY = 0,
+  LAYER_NUMBER,
+  LAYER_SYMBOL,
+  LAYER_NAVI,
+  LAYER_POINTER,
+  LAYER_GAMING
+};
+
+// Automatically enable sniping-mode on the pointer layer.
+#define AUTO_SNIPING_ON_LAYER LAYER_POINTER
+
+#ifdef AUTO_POINTER_LAYER_TRIGGER_ENABLE
+static uint16_t auto_pointer_layer_timer = 0;
+#    ifndef AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
+#        define AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
+#    endif // AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
+
+#    ifndef AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
+#        define AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
+#    endif // AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
+#endif     // AUTO_POINTER_LAYER_TRIGGER_ENABLE
+
+#if defined(ENCODER_MAP_ENABLE)
+#define ENCODER_MAP_KEY_DELAY 10
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }
+};
+#endif
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* LAYER_QWERTY 0: default layer
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ' |
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ Z │ X │ C │ V │ B | N | M | , | . | / |
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │   │   │SHT│SPC│PAU|BSC|DEL|CTL|   |   |
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [LAYER_QWERTY] = LAYOUT(
+        KC_1, KC_2, KC_3,    KC_4,   KC_5,    KC_6,    KC_7,   KC_8,     KC_9,   KC_0,
+        KC_Q, KC_W, KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,   KC_P,
+        KC_A, KC_S, KC_D,    KC_F,   KC_G,    KC_H,    KC_J,   KC_K,     KC_L,   KC_QUOTE,
+        KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_N,    KC_M,   KC_COMMA, KC_DOT, KC_SLASH,
+                    KC_LSFT, KC_SPC, KC_MPLY, KC_BSPC, KC_DEL, KC_LCTL
+    )
+};
